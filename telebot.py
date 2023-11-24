055935aeb71cf14b301174950c6e3d8e998e13ef
Vaishnu1301 <vaishnavigitaje47@gmail.com>
2023-11-25 01:25:38 +0530

    update

--- telebot.py
+++ telebot.py
@@ -3,6 +3,8 @@ import os
 from aiogram import Bot, Dispatcher, executor, types
 import openai
 import sys
+import ssl
+##print(ssl.OPENSSL_VERSION)
 
 class Reference:
     '''
@@ -21,7 +23,7 @@ reference = Reference()
 TOKEN = os.getenv("TOKEN")
 
 #model name
-MODEL_NAME = "gpt-3.5-turbo"
+MODEL_NAME = "OpenSSL 1.1.1+"
 
 
 # Initialize bot and dispatcher
@@ -41,7 +43,51 @@ async def welcome(message: types.Message):
     """
     This handler receives messages with `/start` or  `/help `command
     """
-    await message.reply("Hi\nI am Tele Bot!\Created by Bappy. How can i assist you?")
+    await message.reply("Hi\nI am Tele Bot!\Created by Vaishu. How can i assist you?")
+
+
+@dispatcher.message_handler(commands=['clear'])
+async def clear(message: types.Message):
+    """
+    A handler to clear the previous conversation and context.
+    """
+    clear_past()
+    await message.reply("I've cleared the past conversation and context.")
+
+
+
+@dispatcher.message_handler(commands=['help'])
+async def helper(message: types.Message):
+    """
+    A handler to display the help menu.
+    """
+    help_command = """
+    Hi There, I'm chatGPT Telegram bot created by Vaishu! Please follow these commands - 
+    /start - to start the conversation
+    /clear - to clear the past conversation and context.
+    /help - to get this help menu.
+    I hope this helps. :)
+    """
+    await message.reply(help_command)
+
+
+
+@dispatcher.message_handler()
+async def chatgpt(message: types.Message):
+    """
+    A handler to process the user's input and generate a response using the chatGPT API.
+    """
+    print(f">>> USER: \n\t{message.text}")
+    response = openai.ChatCompletion.create(
+        model = MODEL_NAME,
+        messages = [
+            {"role": "assistant", "content": reference.response}, # role assistant
+            {"role": "user", "content": message.text} #our query 
+        ]
+    )
+    reference.response = response['choices'][0]['message']['content']
+    print(f">>> chatGPT: \n\t{reference.response}")
+    await bot.send_message(chat_id = message.chat.id, text = reference.response)
 
 
 
